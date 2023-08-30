a259e310ffea159afd6d3b09d8a8dd80d9f1ef06
harry <harry_emmett@outlook.com>
2023-08-31 00:17:03 +0100

    calculator functions

---
+++ calculator.js
@@ -0,0 +1,33 @@
+const add = (num, num2) => {
+  return num + num2;
+};
+
+const subtract = (num, num2) => {
+  return num - num2;
+};
+
+const divide = (num, num2) => {
+  return num2 === 0 ? "You can't divide by 0" : num / num2;
+};
+
+const multiply = (num, num2) => {
+  return num * num2;
+};
+
+const operate = (operater, num, num2) => {
+  const castNum = Number(num);
+  const castNum2 = Number(num2);
+
+  switch (operater) {
+    case "+":
+      return add(castNum, castNum2);
+    case "-":
+      return subtract(castNum, castNum2);
+    case "/":
+      return divide(castNum, castNum2);
+    case "*":
+      return multiply(castNum, castNum2);
+    default:
+      return "Invalid operation";
+  }
+};
--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-console.log("hello world");
+console.log(operate("+", 10, 1));
+console.log(operate("-", 10, 1));
+console.log(operate("/", 10, 10));
+console.log(operate("/", 10, 0));
+console.log(operate("*", 10, 10));
+console.log(operate("ooo", 10, 1));
